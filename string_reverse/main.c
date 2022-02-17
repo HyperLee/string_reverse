78f789ecacb55b383c61d662737a0e8731de1fda
Peter.Chiu <tidub76217@hotmail.com>
2022-02-18 00:37:45 +0800

    Update main.c

--- string_reverse/main.c
+++ string_reverse/main.c
@@ -6,9 +6,31 @@
 //
 
 #include <stdio.h>
+#include "stdlib.h"
+void reverse(char str[]);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+    char str[50];
+    printf("input string:");
+    //gets(str);
+    fgets(str, (sizeof str / sizeof str[0]), stdin);
+    reverse(str);
+    
     return 0;
+}
+
+void reverse(char str[]){
+    int i = 0, j = 0;
+    while(str[i] !='\0')
+    {
+        i++;
+    }
+    printf("reverse: ");
+    for(j = i - 1; j >=0; j--)
+    {
+        printf("%c",str[j]);
+        
+    }
+    printf("\n");
 }
